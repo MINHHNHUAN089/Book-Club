7eaaae3eb471f5fd4260b5c3333d646865e9bb73
23050103 <23050103@student.bdu.edu.vn>
2025-12-12 12:15:21 +0700

    BE

--- AA/Frontend/vite.config.ts
+++ AA/Frontend/vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true, // Cho phép truy cập từ mạng local
     port: 5173
   }
 });
